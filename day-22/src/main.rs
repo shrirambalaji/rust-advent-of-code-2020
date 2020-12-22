bf2a8cd125c6d7ab9270044f02b461cdd8d9c8f9
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-12-22 20:57:37 +0530

    Refactor day-22; Add tests

--- day-22/src/main.rs
+++ day-22/src/main.rs
@@ -5,7 +5,6 @@ use std::{
 };
 
 type Deck = VecDeque<u32>;
-// type Score = u32;
 
 #[derive(Debug, PartialEq)]
 enum Winner {
@@ -15,24 +14,25 @@ enum Winner {
 
 fn get_score(deck: &VecDeque<u32>) -> u32 {
     let num_cards = deck.len() as u32;
-
     deck.iter()
         .enumerate()
         .fold(0, |acc, (idx, card)| acc + card * (num_cards - idx as u32))
 }
 
+// Returns the hash for any given collection T
 fn get_hash<T: Hash>(deque: &T) -> u64 {
     let mut hasher = DefaultHasher::new();
     deque.hash(&mut hasher);
     hasher.finish()
 }
 
+// Part 01
 fn combat(deck1: &mut Deck, deck2: &mut Deck) -> Winner {
     if deck1.len() != deck2.len() {
         panic!("Players don't have equal number of cards. Invalid Game!")
     }
 
-    let winner = loop {
+    loop {
         if deck1.is_empty() {
             break Winner::Player2;
         } else if deck2.is_empty() {
@@ -49,35 +49,40 @@ fn combat(deck1: &mut Deck, deck2: &mut Deck) -> Winner {
             deck2.push_back(card2);
             deck2.push_back(card1);
         }
-    };
-
-    winner
+    }
 }
 
-// FIXME: This passes tests, but doesn't seem to give the right answer for given input.
+// Part 02
 fn recursive_combat(deck1: &mut Deck, deck2: &mut Deck) -> Winner {
+    // Store the hashes of decks from previous rounds, used for later comparison
     let mut previous_rounds = HashSet::new();
 
-    while let (Some(&card1), Some(&card2)) = (deck1.front(), deck2.front()) {
-        let h1 = get_hash(&deck1);
-        let h2 = get_hash(&deck2);
-        if previous_rounds.contains(&h1) || previous_rounds.contains(&h2) {
-            return Winner::Player1;
+    // Start the Game Loop
+    loop {
+        let deck1_hash = get_hash(&deck1);
+        let deck2_hash = get_hash(&deck2);
+
+        // Check if the current decks have been seen in previous rounds
+        if previous_rounds.contains(&deck1_hash) || previous_rounds.contains(&deck2_hash) {
+            break Winner::Player1;
         } else {
-            previous_rounds.insert(h1);
-            previous_rounds.insert(h2);
+            previous_rounds.insert(deck1_hash);
+            previous_rounds.insert(deck2_hash);
         }
 
-        deck1.pop_front();
-        deck2.pop_front();
+        // Get the cards from the top of the deck for comparison
+        let card1 = deck1.pop_front().unwrap();
+        let card2 = deck2.pop_front().unwrap();
 
+        // we need to run a subgame to find out the winner
         let winner = if card1 <= deck1.len() as u32 && card2 <= deck2.len() as u32 {
             let mut sub_deck1 = deck1.clone();
             sub_deck1.truncate(card1 as usize);
 
-            let mut sub_deck2 = deck1.clone();
-            sub_deck2.truncate(card1 as usize);
+            let mut sub_deck2 = deck2.clone();
+            sub_deck2.truncate(card2 as usize);
 
+            // recursively run a subgame to find out the winner for the current round
             recursive_combat(&mut sub_deck1, &mut sub_deck2)
         } else if card1 > card2 {
             Winner::Player1
@@ -85,6 +90,7 @@ fn recursive_combat(deck1: &mut Deck, deck2: &mut Deck) -> Winner {
             Winner::Player2
         };
 
+        // based on who won the round, the cards are moved to round winner's deck
         match winner {
             Winner::Player1 => {
                 deck1.push_back(card1);
@@ -95,12 +101,15 @@ fn recursive_combat(deck1: &mut Deck, deck2: &mut Deck) -> Winner {
                 deck2.push_back(card1);
             }
         }
-    }
 
-    if deck1.is_empty() {
-        Winner::Player2
-    } else {
-        Winner::Player1
+        // if any one of the decks are empty, declare the winner
+        if deck1.is_empty() {
+            break Winner::Player2;
+        }
+
+        if deck2.is_empty() {
+            break Winner::Player1;
+        }
     }
 }
 
@@ -151,10 +160,12 @@ fn main() {
     };
     println!("Recursive Combat Winner: {:?}, Score: {:?}", winner, score);
 }
-
-#[test]
-fn should_recursively_combat() {
-    let input = r#"Player 1:
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn should_combat() {
+        let input = r#"Player 1:
 9
 2
 6
@@ -167,8 +178,30 @@ Player 2:
 4
 7
 10"#;
-    let (mut player1_deck, mut player2_deck) = get_decks(&input);
-    let winner = recursive_combat(&mut player1_deck, &mut player2_deck);
-    assert_eq!(winner, Winner::Player2);
-    assert_eq!(get_score(&player2_deck), 291);
+        let (mut player1_deck, mut player2_deck) = get_decks(&input);
+        let winner = combat(&mut player1_deck, &mut player2_deck);
+        assert_eq!(winner, Winner::Player2);
+        assert_eq!(get_score(&player2_deck), 306);
+    }
+
+    #[test]
+    fn should_recursively_combat() {
+        let input = r#"Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10"#;
+        let (mut player1_deck, mut player2_deck) = get_decks(&input);
+        let winner = recursive_combat(&mut player1_deck, &mut player2_deck);
+        assert_eq!(winner, Winner::Player2);
+        assert_eq!(get_score(&player2_deck), 291);
+    }
 }
